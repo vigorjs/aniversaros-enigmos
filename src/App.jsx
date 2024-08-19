58db16e936f64a84f73712a15f42f4652e3aceef
nurdiansyah15 <nurdinurdiansyah15@gmail.com>
2024-08-19 11:56:11 +0700

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,13 @@
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
+import Header from "./components/header/Header";
 
 function App() {
-  // const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <h1 className='font-bold'>Nudros</h1>
+        <Header />
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
