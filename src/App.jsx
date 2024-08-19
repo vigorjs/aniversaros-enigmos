56bd62d0cac788889e2dafbf5e29afe11179c846
vigorjs <111201912289@mhs.dinus.ac.id>
2024-08-19 20:38:07 +0700

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,6 @@ import Hero from "./components/hero/Hero";
 import { useState, useEffect } from "react";
 import SpinnerLoadingScreen from "./components/ui/SpinnerLoading/SpinnerLoadingScreen";
 import About from "./components/about/About";
-
-const images = [
-  "https://via.placeholder.com/400x400?text=Image+1",
-  "https://via.placeholder.com/400x400?text=Image+2",
-  "https://via.placeholder.com/400x400?text=Image+3",
-  "https://via.placeholder.com/400x400?text=Image+4",
-  "https://via.placeholder.com/400x400?text=Image+5",
-];
-
 function App() {
   
   const [loading, setLoading] = useState(true);
@@ -29,8 +20,8 @@ function App() {
     <>
        <SpinnerLoadingScreen loading={loading} />
       <div className={`App ${loading ? 'hidden' : ''}`}>
-        {/* <Header/>
-        <Hero/> */}
+        <Header/>
+        <Hero/>
         <About/>
       </div>
     </>
